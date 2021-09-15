7bd08fa0323cfa537c4aca9db513fe4b16e9774f
Gospel Chukwu <brondchux@gmail.com>
2021-09-14 22:40:31 -0400

    Test commit to fix npm run seed on production

--- seeders/seed.js
+++ seeders/seed.js
@@ -125,8 +125,19 @@ const workoutSeed = [
 	},
 ];
 
-db.Workout.deleteMany({})
-	.then(() => db.Workout.collection.insertMany(workoutSeed))
+// db.Workout.deleteMany({})
+// 	.then(() => db.Workout.collection.insertMany(workoutSeed))
+// 	.then((data) => {
+// 		console.log(data.result.n + " records inserted!");
+// 		process.exit(0);
+// 	})
+// 	.catch((err) => {
+// 		console.error(err);
+// 		process.exit(1);
+// 	});
+
+db.Workout.collection
+	.insertMany(workoutSeed)
 	.then((data) => {
 		console.log(data.result.n + " records inserted!");
 		process.exit(0);
